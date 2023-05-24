e3f4103149f489dee2c8718e11a57c245db62cd5
Marcel Fernandez Rosas <60543555+Ijusttyped@users.noreply.github.com>
2023-05-24 10:15:48 +0200

    Minor updates

--- scripts/run_fastsurfer.sh
+++ scripts/run_fastsurfer.sh
@@ -6,12 +6,12 @@ files_found=$(find "${1:-data}" -name "${IN_FILENAME}" -print0 | xargs -0)
 for file in $files_found; do
   echo "Processing ${file}"
 
-  out_dir="$(dirname ${file})/"
+  out_dir="$(dirname "${file}")/"
 
-  if [ ! -f ${out_dir}/fastsurfer_out/stats/aparc.DKTatlas+aseg.deep.volume.stats ]; then
+  if [ ! -f "${out_dir}/fastsurfer_out/stats/aparc.DKTatlas+aseg.deep.volume.stats" ]; then
     docker run  -v "${PWD}"/data:/data \
                 -v "${PWD}"/freesurfer_license:/fs_license \
-                --rm --user $(id -u):$(id -g) \
+                --rm --user "$(id -u):$(id -g)" \
                 deepmi/fastsurfer:latest \
                 --fs_license /fs_license/license.txt \
                 --sid fastsurfer_out \
